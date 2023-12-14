d66a612b760653fbf5a0b0cecd2f909ad761f7e7
leechinghua <leechenghua107@gmail.com>
2023-12-14 23:35:26 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
@@ -14,7 +16,7 @@ app.get('/',(req,res)=>{
   res.redirect('/restaurants')
 })
 app.get('/restaurants', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurants})
 })
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
